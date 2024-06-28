d3eafb01b0cc9d6481d24df336d91e146fee9702
ndrbn0 <andrea.bruni1992@gmail.com>
2024-06-28 14:42:59 +0200

    created a img placeholder and styled the ingredients cards.

--- components/IngredientsList.js
+++ components/IngredientsList.js
@@ -1,6 +1,7 @@
 import ingredients from "@/assets/ingredients.json";
 //import flavor from "@/assets/flavors.json";
 import styled from "styled-components";
+import Image from "next/image";
 
 const IngredientsList = () => {
   const flavorColors = {
@@ -18,27 +19,37 @@ const IngredientsList = () => {
     Umami: "#188989",
   };
   return (
-    <Container>
+    <>
       <Title>Ingredients Overview</Title>
-      <List>
-        {ingredients.map((ingredient) => (
-          <ListItem
-            key={ingredient._id}
-            flavor={ingredient.flavor}
-            color={flavorColors[ingredient.flavor]}
-          >
-            <Name>{ingredient.name}</Name>
-            <Flavor>{ingredient.flavor}</Flavor>
-          </ListItem>
-        ))}
-      </List>
-    </Container>
+      <Container>
+        <List>
+          {ingredients.map((ingredient) => (
+            <ListItem
+              key={ingredient._id}
+              flavor={ingredient.flavor}
+              color={flavorColors[ingredient.flavor]}
+            >
+              <Image1 />
+              <Name>{ingredient.name}</Name>
+              <Flavor>{ingredient.flavor}</Flavor>
+            </ListItem>
+          ))}
+        </List>
+      </Container>
+    </>
   );
 };
 const Container = styled.div`
-  padding: 20px;
+  /* padding: 20px;
   max-width: 600px;
-  margin: auto;
+  margin: auto; */
+  width: calc(100% - 20px);
+  height: 60%;
+  padding: 10px;
+  display: flex;
+  box-sizing: border-box;
+  border-radius: 10px;
+  overflow: hidden;
 `;
 const Title = styled.h1`
   text-align: center;
@@ -47,14 +58,40 @@ const Title = styled.h1`
 const List = styled.ul`
   list-style: none;
   padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+
+  align-self: stretch;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
 `;
 const ListItem = styled.li`
-  display: flex;
-  justify-content: space-between;
+  width: 280px;
+  height: 340px;
+  margin: 20px;
+  padding: 5px;
+  border-radius: 10px;
+  justify-content: flex-start;
   background-color: ${(props) => props.color || "#fff"};
-  padding: 10px;
-  margin-bottom: 10px;
-  border-radius: 5px;
+  border-radius: 15px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 6px 20px rgba(0, 0, 0, 0.2);
+  }
 `;
 const Name = styled.span`
   font-weight: bold;
@@ -62,5 +99,22 @@ const Name = styled.span`
 const Flavor = styled.span`
   font-style: italic;
 `;
-
+const Image1 = styled.div`
+  align-self: stretch;
+  height: 210px;
+  overflow: hidden;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  box-sizing: border-box;
+  background-image: url("assets/Apfel.png.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: top;
+  /* border: 1px solid black; */
+  background-color: #fff;
+  border-radius: 10px;
+`;
 export default IngredientsList;
