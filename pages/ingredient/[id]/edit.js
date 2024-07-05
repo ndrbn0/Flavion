1b515b10db42afd7839311298979c48c1ba1d7f9
Andrea Bruni <168083201+ndrbn0@users.noreply.github.com>
2024-07-05 10:22:06 +0200

    Update pages/ingredient/[id]/edit.js
    Co-authored-by: Maren Ehlers <152329875+MarenOelixtown@users.noreply.github.com>

--- pages/ingredient/[id]/edit.js
+++ pages/ingredient/[id]/edit.js
@@ -1,6 +1,6 @@
 import EditIngredient from "@/components/EditIngredient";
 import { useRouter } from "next/router";
-import ingredientsData from "@/assets/ingredients.json"; // Assuming this is where you store your ingredients data
+import ingredientsData from "@/assets/ingredients.json"; 
 
 const EditIngredientPage = (props) => {
   const router = useRouter();
