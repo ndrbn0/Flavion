376cfc79ff20cc7438e3d94c0d6ab0a086f45f4d
ndrbn0 <andrea.bruni1992@gmail.com>
2024-07-09 11:38:38 +0200

    Pairing cards styling has been adjusted, json has been updated, url upcoming

--- components/PairingItem.js
+++ components/PairingItem.js
@@ -1,9 +1,33 @@
-import { Pairing, Ingredients, Reason } from "@/_styles";
+import {
+  Pairing,
+  Ingredients,
+  Reason,
+  ImageWrapper,
+  StyledImage,
+} from "@/_styles";
+import ingredientsData from "@/assets/ingredients.json";
 
 const PairingItem = ({ pairing }) => {
+  const getIngredientNames = (ingredientIds) => {
+    return ingredientIds.map((id) => {
+      const ingredient = ingredientsData.find((ing) => ing._id === id);
+      return ingredient ? ingredient.name : "Unknown";
+    });
+  };
+
   return (
     <Pairing>
-      <Ingredients>{pairing.ingredients.join(", ")}</Ingredients>
+      <ImageWrapper>
+        <StyledImage
+          src={pairing.imgUrl}
+          alt={pairing.name}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </ImageWrapper>
+      <Ingredients>
+        {getIngredientNames(pairing.ingredients).join(", ")}
+      </Ingredients>
       <Reason>{pairing.reason}</Reason>
     </Pairing>
   );
